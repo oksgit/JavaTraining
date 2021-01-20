ba552c6851d751789af723e89cf59547c125a2ab
oksgit <oksdem@gmail.com>
2021-01-20 23:53:48 +0300

    commit 1

---
+++ src/main/java/Homework_Evroopt/Main.java
@@ -0,0 +1,8 @@
+package Homework_Evroopt;
+
+public class Main {
+    public static void main(String [] arg){
+        System.out.println("Homework_Evroopt downloaded into git_rep");
+        System.out.println("updated");
+    }
+}
