985195a9b9d200cbf126548a9e76b4fe5b1c195b
oksgit <oksdem@gmail.com>
2021-01-21 10:57:32 +0300

    pom updated

--- src/main/java/Homework_Evroopt/Test.java
+++ src/main/java/Homework_Evroopt/Test.java
@@ -1,8 +1,13 @@
 package Homework_Evroopt;
 
-public class Main {
+public class Test {
     public static void main(String [] arg){
         System.out.println("Homework_Evroopt downloaded into git_rep");
-        System.out.println("updated");
+
+        public void checkPrice();
     }
+    public void checkPrice(){
+        open("https://evroopt.by/");
+    }
+
 }
