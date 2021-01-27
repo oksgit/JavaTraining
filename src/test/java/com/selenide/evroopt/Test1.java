0ce9621eed1eba7ce879adee4c4a369654e3bc7c
oksgit <oksdem@gmail.com>
2021-01-27 19:14:14 +0300

    123

---
+++ src/test/java/com/selenide/evroopt/Test1.java
@@ -0,0 +1,20 @@
+package com.selenide.evroopt;
+
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class Test1 {
+    @Test
+    public void checkPrice(){
+
+        open ("https://evroopt.by/");
+        //$(By.xpath ("div.[@class='main_menu__inner']//i[@class='fa fa-search']"));
+
+    }
+
+}
+
