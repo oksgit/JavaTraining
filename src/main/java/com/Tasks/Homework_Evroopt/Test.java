d15b7db998c9d8ec5ecc062db0c68a439112f0df
oksgit <oksdem@gmail.com>
2021-01-27 19:14:14 +0300

    123

---
+++ src/main/java/com/Tasks/Homework_Evroopt/Test.java
@@ -0,0 +1,12 @@
+package com.Tasks.Homework_Evroopt;
+
+public class Test {
+
+    public void checkPrice(){
+
+        open ("https://evroopt.by/");
+        $(By.xpath ("div.[@class='main_menu__inner']//i[@class='fa fa-search']"));
+
+    }
+
+}
